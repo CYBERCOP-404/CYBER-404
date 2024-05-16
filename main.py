605569a9992bdd6e2e938a587718a224a62d616d
CYBER-COP-BANGLADESH <168484308+CYBERCOP-404@users.noreply.github.com>
2024-05-17 01:44:42 +0600

    new phone verson .
    new phone verson 2.2

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-# import section Start
-import os 
-from time import sleep
-# import section end
-def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
-BANNER='''\033[0;32m
+BANNER = '''\033[0;32m
  $$$$$$\ $$\     $$\ $$$$$$$\  $$$$$$$$\ $$$$$$$\ 
 $$  __$$  $$\   $$  |$$  __$$\ $$  _____|$$  __$$\  
 $$ /  \__|\$$\ $$  / $$ |  $$ |$$ |      $$ |  $$ | 
@@ -14,29 +8,60 @@ $$ |  $$\    $$ |    $$ |  $$ |$$ |      $$ |  $$ |
 \$$$$$$  |   $$ |    $$$$$$$  |$$$$$$$$\ $$ |  $$ |  
  \______/    \__|    \_______/ \033[0;31m PHONE VERSON > 2.2
 \033[0;32m
+ [1] DO YOU NEED ANY GIRL FRIEND 
+ [2] DO YOU NEED MANY GIRL FRIEND  
+ [3] DO YOU WANT TO EXIT 
 '''
-command_list='''
-[1] USE GF FINDER PHONE VERSON 
-[2] USE THIS GF FINDER TOOL 
+comm='''
+Login error.........
 '''
-comm ='''\033[0;31m
-LOGIN ERROR ....
-'''
+# girl section start 
+sonia   = 'https://www.facebook.com/profile.php?id=61555730134982'
+sadia   = 'https://www.facebook.com/profile.php?id=61550780090307'
+nusrat  = 'https://www.facebook.com/profile.php?id=61559358894560'
+rani    = 'https://www.facebook.com/nilaaktar.nilisha'
+maisa   = 'https://www.facebook.com/misha.moon1'
+nusu    = 'https://www.facebook.com/profile.php?id=61558430423969'
+mahiya  = 'https://www.facebook.com/profile.php?id=61557292204247'
+sanjida = 'https://www.facebook.com/profile.php?id=100092753300663'
+raisa   = 'https://www.facebook.com/profile.php?id=61552388255134'
+israt   = 'https://www.facebook.com/profile.php?id=61554654218911'
+# girl section end 
+girl_list =[sonia,sadia,nusrat,rani,maisa,nusu,mahiya,sanjida,raisa,israt]
+# import section start.  
+from random2 import choice
+from time import sleep
 while True:
-    clear_screen()
-    print(BANNER)
-    print(command_list)
-    CHOICE = input('\033[1;34mENTER YOUR CHOICE : ')
-    if CHOICE =='1':
-        os.system('python gf_fiender.py')
-    elif CHOICE=='2':
-        os.system('python gf.py')
-    else:
-        for i in range(10,0,-1):
-            sleep(0.5)
-            clear_screen()
-            print(BANNER)
-            print(comm)
-            print(f'TRY AFTER {i} SECOND')
-            sleep(0.5)
-# Developed by CYBER-COP-BANGLADESH
+	MAME = input('WHAT IS YOUR NAME : ')
+	NAME = MAME.upper()
+	print(BANNER)
+	ino = input('WHAT IS YOUR CHOICE : ')
+	if ino =='1':
+			ok = choice(girl_list)
+			print('Congratulation......  You have now girl friend > ')
+			print(ok)
+			print()
+			print()
+			exit = input('DO YOU WANT TO EXIT NOW ?  PRESS ENTER')
+			if exit =='1':
+				print('HELLO  CYBER-COP')
+			else:
+				break
+	elif ino =='2':
+			print('FUCK YOU......  YOU ARE FUCK BOY........ THATS WHY YOU ARE SINGLE......  FUCK YOU  '+NAME)
+			INPUTO = input('DO YOU WANT TO EXIT..... PRESS ENTER')
+			if INPUTO =='1':
+				print('HELLO CYBER-COP')
+			else:
+				break
+	elif ino =='3':
+		break
+		 
+				
+				
+			
+
+
+
+
+
