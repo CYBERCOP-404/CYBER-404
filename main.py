5e039bd25a2fdfeea238f3fb276dbf1dcccc4a25
CYBER-COP <168484308+CYBERCOP-404@users.noreply.github.com>
2024-05-20 20:34:13 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os 
 from time import sleep
 # import section end
+A = '\x1b[1;93m';Y = '\033[1;33m';G = '\033[1;96m';R = '\x1b[38;5;196m';B = '\x1b[38;5;8m';G1 = '\x1b[38;5;48m';G2 = '\x1b[38;5;47m';G3 = '\x1b[38;5;48m';X1 = '\x1b[38;5;14m';X2 = '\x1b[38;5;123m';X5 = '\x1b[38;5;121m'
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 BANNER='''\033[0;32m
@@ -13,6 +14,14 @@ $$ |        \$$  /   $$  __$$\ $$  __|   $$  __$$<
 $$ |  $$\    $$ |    $$ |  $$ |$$ |      $$ |  $$ |  
 \$$$$$$  |   $$ |    $$$$$$$  |$$$$$$$$\ $$ |  $$ |  
  \______/    \__|    \_______/ \033[0;31m PHONE VERSON > 2.3
+\033[1;39m ┏━━━━━━━━━━━━━━━━━━━\033[38;5;46m𝗞𝗜𝗡𝗚\033[1;39m━━━━━━━━━━━━━━━━━━━━━━━━┓
+\033[1;39m ┃ \x1b[1;95m[💉]😎\x1b[1;96m 𝙉𝘼𝙈𝙀\033[1;34m       : [★]  CYBER COP BANGLADESH\033[1;39m ┃
+\033[1;39m ┃ \x1b[1;95m[💉]😎\x1b[1;96m 𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆\033[1;34m   : [★]  CYBER COP\033[1;39m            ┃
+\033[1;39m ┃ \x1b[1;95m[💉]😎\x1b[1;96m 𝙂𝙄𝙏𝙃𝙐𝘽\033[1;34m     : [★]  CYBERCOP-404\033[1;39m         ┃
+\033[1;39m ┃ \x1b[1;95m[💉]😎\x1b[1;96m 𝙍𝙄𝙇𝙄𝙂𝙀𝙎𝙃𝙊𝙉\033[1;34m : [★]  𝗕𝗔𝗡𝗚𝗟𝗔𝗗𝗘𝗦𝗛𝗜\033[1;39m          ┃
+\033[1;39m ┃ \x1b[1;95m[💉]😎\x1b[1;96m 𝙒𝙃𝘼𝙏𝙎𝘼𝙋𝙋\033[1;34m   : [★]  +8809638223345\033[1;39m       ┃
+\033[1;39m ┃ \x1b[1;95m[💉]😎\x1b[1;96m 𝙏𝙊𝙊𝙇𝙎 𝙉𝘼𝙈𝙀\033[1;31m : [★]  MAIN-ROOT-CYBER     \033[1;39m ┃
+ \033[1;39m┗━━━━━━━━━━━━━━━━━━━\033[1;32m 𝙁𝙄𝙍𝙀\033[1;39m━━━━━━━━━━━━━━━━━━━━━━━┛
 \033[0;32m
 '''
 command_list='''
@@ -24,38 +33,58 @@ command_list='''
 [6] ENC-ENCRIPTED CODE
 [7] EXIT PROGRAM
 '''
+url ='https://www.github.com/cybercop-404'
 comm ='''\033[0;31m
 LOGIN ERROR ....
 '''
-while True:
-    clear_screen()
-    print(BANNER)
-    print(command_list)
-    CHOICE = input('\033[1;34m ENTER YOUR CHOICE : ')
-    if CHOICE =='2':
-        os.system('python fb-report.py')
-    elif CHOICE=='3':
-        os.system('python gf_bf.py')
-    elif CHOICE=='4':
-        os.system('python virus.py')
-    elif CHOICE=='5':
-        os.system('python game.py')
-    elif CHOICE=='11221':
-        os.system('python g.py')
-    elif CHOICE=='119887':
-        os.system('python marshal_admin.py')
-    elif CHOICE=='1':
-        os.system('python setup.py')
-    elif CHOICE=='6':
-        os.system('python marshal.py')
-    elif CHOICE=='7':
-        break
-    else:
-        for i in range(10,0,-1):
-            sleep(0.5)
-            clear_screen()
-            print(BANNER)
-            print(comm)
-            print(f'TRY AFTER {i} SECOND')
-            sleep(0.5)
+print(BANNER)
+print('\033[1;34m [+] ENTER YOUR PASSWORD :')
+print(' [1] DO YOU WANT TO BUY THE TOOL : ')
+pass_ok=input(f'{A}【•】CHOICE ➤ \033[1;32m ')
+if pass_ok=='1':
+    os.system('clear')
+    print('FOLLOW MY GITHUB..............')
+    os.system('clear')
+    os.system(f'xdg-open {url}')
+elif pass_ok =='119887':
+    while True:
+        clear_screen()
+        print(BANNER)
+        print(command_list)
+        CHOICE = input('\033[1;34m ENTER YOUR CHOICE : ')
+        if CHOICE =='2':
+            os.system('python fb-report.py')
+        elif CHOICE=='3':
+            os.system('python gf_bf.py')
+        elif CHOICE=='4':
+            os.system('python virus.py')
+        elif CHOICE=='5':
+            os.system('python game.py')
+        elif CHOICE=='11221':
+            os.system('python g.py')
+        elif CHOICE=='119887':
+            os.system('python marshal_admin.py')
+        elif CHOICE=='1':
+            os.system('python setup.py')
+        elif CHOICE=='6':
+            os.system('python marshal.py')
+        elif CHOICE=='7':
+            break
+        else:
+            for i in range(10,0,-1):
+                sleep(0.5)
+                clear_screen()
+                print(BANNER)
+                print(comm)
+                print(f'TRY AFTER {i} SECOND')
+                sleep(0.5)
+else:
+    for i in range(10,0,-1):
+        sleep(0.5)
+        clear_screen()
+        print(BANNER)
+        print(comm)
+        print(f'TRY AFTER {i} SECOND')
+        sleep(0.5)
+    os.system('python main.py')
 # Developed by CYBER-COP-BANGLADESH
